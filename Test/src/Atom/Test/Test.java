9518729e58ed3ab936e4268684bf1646213ee24a
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-05 04:15:00 +0200

    WHAT THE F are you doing

--- Test/src/Atom/Test/Test.java
+++ Test/src/Atom/Test/Test.java
@@ -1,43 +1,11 @@
 package Atom.Test;
 
-import Atom.Annotation.Builder.ClassBuilder;
-import Atom.Annotation.Builder.MethodBuilder;
-import Atom.Annotation.Builder.VariableBuilder;
-import Atom.Log.Log;
-import org.checkerframework.checker.units.qual.C;
+
 
 public class Test {
-    MethodBuilder ma = new MethodBuilder("public static void", "name", "", "System.out.println(\"geh\");");
-    MethodBuilder mb = new MethodBuilder(" public static String ", "  a", " String name ", "" +
-            "System.out.println(\"geh\");" +
-            "System.out.println(\"geh\");" +
-            "System.out.println(\"geh\");");
-    VariableBuilder va = new VariableBuilder(" public ", "String ", "str ", "");
-    VariableBuilder vb = new VariableBuilder(" ", " String", "strs ", "Nig");
+
     @org.junit.jupiter.api.Test
     void name() {
     }
 
-    @org.junit.jupiter.api.Test
-    public void VariableBuilderTest() {
-
-        Log.info(va.toString());
-        Log.info(vb.toString());
-    }
-    @org.junit.jupiter.api.Test
-    public void MethodBuilderTest(){
-
-        Log.info(ma.toString());
-        Log.info(mb.toString());
-    }
-    @org.junit.jupiter.api.Test
-    public void ClassBuilderTest() {
-        ClassBuilder cb = new ClassBuilder("public class", "yes");
-        cb.methodList.add(ma);
-        cb.methodList.add(mb);
-        cb.variableList.add(va);
-        cb.variableList.add(vb);
-        cb.addData("static{}");
-        Log.info(cb.toString());
-    }
 }
--- Test/src/Person.java
+++ Test/src/Person.java
@@ -1,21 +1,9 @@
-import Atom.Annotation.Antonation;
+
 
 public class Person {
 
-    private int age;
 
-    private String name;
 
-    @Antonation.BuilderProperty
-    public void setAge(int age) {
-        this.age = age;
-    }
 
-    @Antonation.BuilderProperty
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // getters …
 
 }
