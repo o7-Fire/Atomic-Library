1152638a4cc1aafe662cf2ef5a1b4b9832432d3c
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-10 13:42:37 +0200

    mindustry add-on

---
+++ Mindustry/src/Atom/Mindusrty.java
@@ -0,0 +1,20 @@
+package Atom;
+
+import Atom.Utility.Utility;
+import arc.util.Log;
+import mindustry.mod.Mod;
+
+public class Mindusrty  extends Mod {
+    @Override
+    public void init() {
+        Random.getBool();
+        Utility.getJavaMajorVersion();
+        Log.infoTag("Atom", "Loaded");
+        super.init();
+    }
+
+    @Override
+    public void loadContent() {
+        super.loadContent();
+    }
+}
