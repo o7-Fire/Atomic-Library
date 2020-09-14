5958a3941371520873f5799ce27e0adcff510412
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-14 07:12:35 +0200

    god

--- Aindustry/src/Atom/Aindustry.java
+++ Aindustry/src/Atom/Aindustry.java
@@ -1,32 +1,22 @@
 package Atom;
 
-import Atom.Classloader.SystemURLClassLoader;
-import Atom.Reflect.Reflect;
-import Atom.Struct.Filter;
 import arc.util.Log;
 import mindustry.mod.Mod;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URLClassLoader;
-
 public class Aindustry extends Mod {
+
     @Override
     public void init() {
         try {
-            Reflect.findPackages(new Filter<Package>() {
-                @Override
-                public boolean accept(Package aPackage) {
-                    return false;
-                }
-            });
-            SystemURLClassLoader.loadJar((URLClassLoader) this.getClass().getClassLoader(), Reflect.getCurrentJar(this.getClass()));
+            if(!Manifest.checkIntegrity()){
+                Log.infoTag("Atom", "Nope");
+                return;
+            }
             Log.infoTag("Atom", "Loaded");
         } catch (Throwable e) {
             e.printStackTrace();
             Log.err(e);
         }
-
     }
 
 }
--- Atomic/src/Atom/Manifest.java
+++ Atomic/src/Atom/Manifest.java
@@ -1,6 +1,11 @@
 package Atom;
 
 
+import Atom.Reflect.Reflect;
+
 public class Manifest {
 
+    public static boolean checkIntegrity(){
+        return Reflect.getExtendedClass("Atom", java.lang.Object.class).size() > 1;
+    }
 }
