1a1acff1f7f4c0074a2771782b4bd7f032118665
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-17 06:08:16 +0200

    Create Meth.java

---
+++ Atomic/src/Atom/Meth.java
@@ -0,0 +1,13 @@
+package Atom;
+
+public class Meth {
+
+    public static int positive(int i){
+        if(i < 0)return -i;
+        return i;
+    }
+    public static int negative(int i){
+        if(i > 0)return -i;
+        return i;
+    }
+}
