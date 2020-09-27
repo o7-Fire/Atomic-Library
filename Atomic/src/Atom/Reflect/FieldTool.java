feafa6daea6758c4c3edf9301d84786c89cec550
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-27 09:30:20 +0200

    Field

--- Atomic/src/Atom/Reflect/FieldTool.java
+++ Atomic/src/Atom/Reflect/FieldTool.java
@@ -3,9 +3,35 @@ package Atom.Reflect;
 import Atom.Random;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+
+import static Atom.Reflect.Reflect.findDeclaredField;
 
 public class FieldTool {
 
+    public static void setFinalStatic(Field field, Object newValue) throws Exception {
+        field.setAccessible(true);
+
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+        field.set(null, newValue);
+    }
+
+    public static Field getField(Class<?> clazz, String name, Object object) {
+        ArrayList<Field> result = findDeclaredField(clazz, f -> f.getName().equals(name), object);
+        Field e = null;
+        if (result == null) return null;
+        for (Field f : result) {
+            try {
+                e = f;
+            } catch (Throwable ignored) {
+            }
+        }
+        return e;
+    }
 
     public static void assignRandom(Field[] fields) {
         for (Field f : fields) {
