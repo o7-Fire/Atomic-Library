20ffd9943167cee601563207349f1f98f2f0a62e
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-10-07 06:30:55 +0200

    ugh

--- Atomic/src/Atom/Meth.java
+++ Atomic/src/Atom/Meth.java
@@ -1,6 +1,28 @@
 package Atom;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Meth {
+
+    public static long max(Long[] arr) {
+        List<Long> b = Arrays.asList(arr);
+        return Collections.max(b);
+    }
+
+    public static long min(Long[] arr) {
+        List<Long> b = Arrays.asList(arr);
+        return Collections.min(b);
+    }
+
+    public static double avg(long[] arr) {
+        double sum = 0;
+        for (long l : arr) {
+            sum += l;
+        }
+        return sum / arr.length;
+    }
 
     public static int positive(int i) {
         if (i < 0) return -i;
