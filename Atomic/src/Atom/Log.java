392f372d041c376be3466115a80cc1fbd848b755
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-10-09 04:35:29 +0200

    Cleaning and fixing

---
+++ Atomic/src/Atom/Log.java
@@ -0,0 +1,46 @@
+package Atom;
+
+public abstract class Log {
+    protected String[] sLevel = new String[]{"[Debug]", "[Info]", "[Warn]", "[Error]"};
+
+
+    protected void output(Object raw, int level) {
+        System.out.println(sLevel[level] + " " + raw);
+    }
+
+    protected void output(String tag, Object raw, int level) {
+        System.out.println(sLevel[level] + " " + tag + ": " + raw);
+    }
+
+    public void debug(Object s) {
+        output(s, 0);
+    }
+
+    public void info(Object s) {
+        output(s, 1);
+    }
+
+    public void warn(Object s) {
+        output(s, 2);
+    }
+
+    public void err(Object s) {
+        output(s, 3);
+    }
+
+    public void debug(String tag, Object s) {
+        output(tag, s, 0);
+    }
+
+    public void info(String tag, Object s) {
+        output(tag, s, 1);
+    }
+
+    public void warn(String tag, Object s) {
+        output(tag, s, 2);
+    }
+
+    public void err(String tag, Object s) {
+        output(tag, s, 3);
+    }
+}
