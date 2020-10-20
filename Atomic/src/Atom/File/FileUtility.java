dccba77b8b8e2d5d58ac7c04e56a5a63e8a550d3
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-10-20 10:06:07 +0200

    here

--- Atomic/src/Atom/File/FileUtility.java
+++ Atomic/src/Atom/File/FileUtility.java
@@ -3,19 +3,9 @@ package Atom.File;
 import Atom.Manifest;
 
 import java.io.File;
-import java.nio.file.Files;
 
 public class FileUtility {
 
-    public static boolean createFile(File file) {
-        try {
-            Files.createFile(file.toPath());
-            return true;
-        } catch (Throwable ignored) {
-            //probraly already exists
-        }
-        return false;
-    }
 
     public static File temp() {
         File temp = new File(Manifest.currentFolder, System.currentTimeMillis() + ".temp");
