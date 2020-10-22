eada873c4fad0071aec76e7d348690b0e7a78302
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-10-22 13:15:50 +0200

    cheer

---
+++ Atomic/src/Atom/Struct/History.java
@@ -0,0 +1,34 @@
+package Atom.Struct;
+
+public class History<T> {
+    T Old, Neu;
+    long old, neu;
+
+    public History(T type) {
+        Neu = type;
+        neu = System.currentTimeMillis();
+    }
+
+    public T getOld() {
+        return Old;
+    }
+
+    public T getNew() {
+        return Neu;
+    }
+
+    public void setNew(T object) {
+        old = neu;
+        neu = System.currentTimeMillis();
+        Old = Neu;
+        Neu = object;
+    }
+
+    public long getOldTimestamp() {
+        return old;
+    }
+
+    public long getNewTimestamp() {
+        return neu;
+    }
+}
