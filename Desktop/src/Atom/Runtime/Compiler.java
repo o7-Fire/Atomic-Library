2872bfeb863c27e1142bd4780af1d7c327327582
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-02-08 05:32:37 +0100

    h

--- Desktop/src/Atom/Runtime/Compiler.java
+++ Desktop/src/Atom/Runtime/Compiler.java
@@ -5,6 +5,7 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.Statement;
 
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
@@ -12,24 +13,31 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class Compiler {
 	
 	public static final String template = "package Dummy.Test;\n" + "\n" + "public class Test{\n" + "    \n" + "    public Test(){\n" + "        \n" + "    }\n" + "    \n" + "    public void init(){\n" + "        \n" + "    }\n" + "}\n";
+	public static CompilationUnit parsedTemplate = null;
 	
 	//literally every exception
 	public static void runLine(String line, OutputStream out) throws Exception {
+		if (line.isEmpty()) throw new IllegalArgumentException("Empty input");
 		CompilationUnit c = getTestTemplate();
-		c.findFirst(ClassOrInterfaceDeclaration.class).get().findAll(BlockStmt.class).get(1).addStatement(line);
+		Statement statement = StaticJavaParser.parseStatement(line);
+		try {
+			c.findFirst(ClassOrInterfaceDeclaration.class).get().findAll(BlockStmt.class).get(1).addStatement(statement);
+		}catch (Throwable t) {
+			throw new RuntimeException("Flawed template", t);
+		}
 		RuntimeClass rc = compile(out, Manifest.workingDir, c);
 		rc.load();
 		rc.invokeMethod("init");
 	}
 	
 	public static CompilationUnit getTestTemplate() {
-		return StaticJavaParser.parse(template);
+		if (parsedTemplate != null) return parsedTemplate;
+		return parsedTemplate = StaticJavaParser.parse(template);
 	}
 	
 	public static RuntimeClass compile(OutputStream out, File workingDir, CompilationUnit compilationUnit) throws IOException {
@@ -39,9 +47,9 @@ public class Compiler {
 		source.getParentFile().mkdirs();
 		source.delete();
 		compiled.delete();
-		Files.write(source.toPath(), compilationUnit.toString().getBytes(StandardCharsets.UTF_8));
+		Files.writeString(source.toPath(), compilationUnit.toString());
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		compiler.run(null, out, out, source.getAbsolutePath());
+		if (compiler.run(null, out, out, source.getAbsolutePath()) != 0) throw new RuntimeException("Compiler Error");
 		if (!compiled.exists()) throw new FileNotFoundException(compiled.getAbsolutePath() + " not found");
 		return new RuntimeClass(workingDir, classpath);
 	}
