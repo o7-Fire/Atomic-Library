8ef3b92c3872c5d4e4858e8d727c3780e0290e9f
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-03-19 11:59:19 +0100

    retard

--- Atomic/src/Atom/Utility/EncoderJson.java
+++ Atomic/src/Atom/Utility/EncoderJson.java
@@ -10,17 +10,30 @@ import java.util.Map;
 
 public class EncoderJson {
 	
-	public static HashMap<String, String> jsonToMap(JsonObject jo, String prefix) {
+	public static HashMap<String, String> jsonToMap(JsonElement jo, String prefix) {
 		HashMap<String, String> map = new HashMap<>();
-		for (Map.Entry<String, JsonElement> s : jo.entrySet()) {
-			String pre = prefix + "." + s.getKey();
-			if (s.getValue() instanceof JsonPrimitive) map.put(pre, s.getValue().getAsString());
-			if (s.getValue() instanceof JsonObject) map.putAll(jsonToMap((JsonObject) s.getValue(), pre));
+		if(	jo.isJsonNull()){
+			return map;
+		}else if(jo.isJsonObject()) {
+			for (Map.Entry<String, JsonElement> s : jo.getAsJsonObject().entrySet()) {
+				String pre = prefix + "." + s.getKey();
+				if (s.getValue() instanceof JsonPrimitive) map.put(pre, s.getValue().getAsString());
+				if (s.getValue() instanceof JsonObject) map.putAll(jsonToMap(s.getValue(), pre));
+			}
+		}else if(jo.isJsonArray()){
+			int i = 0;
+			for (JsonElement s : jo.getAsJsonArray()) {
+				String pre = prefix + "." + i++;
+				map.putAll(jsonToMap(s));
+			}
+		}else if(jo.isJsonPrimitive()){//in case
+			map.put(prefix, jo.getAsJsonPrimitive().getAsString());
 		}
+	
 		return map;
 	}
 	
-	public static HashMap<String, String> jsonToMap(JsonObject jo) {
+	public static HashMap<String, String> jsonToMap(JsonElement jo) {
 		return jsonToMap(jo, "");
 	}
 	
