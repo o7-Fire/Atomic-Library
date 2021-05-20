a511e5fda973407925d79096849dbb188d5218d1
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-05-20 10:48:35 +0200

    ok and

--- Atomic/src/Atom/Static.java
+++ Atomic/src/Atom/Static.java
@@ -1,7 +1,9 @@
 package Atom;
 
+import Atom.File.RepoInternal;
 import Atom.Utility.Log;
 
 public class Static {//what
     public static Log log = new Log() {};
+    public static RepoInternal resource = Manifest.internalRepo;
 }
