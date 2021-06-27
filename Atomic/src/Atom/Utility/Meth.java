f8ed02d92430ea77a2a3ce7232d924aeae228855
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-06-27 14:05:09 +0200

    mdp dqn inp native bla bla bla

--- Atomic/src/Atom/Utility/Meth.java
+++ Atomic/src/Atom/Utility/Meth.java
@@ -2,6 +2,7 @@ package Atom.Utility;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class Meth {
@@ -14,6 +15,195 @@ public class Meth {
     public static long min(Long[] arr) {
         List<Long> b = Arrays.asList(arr);
         return Collections.min(b);
+    }
+    
+    public static float max(float... arr) {
+        if (arr.length == 0) return 0;
+        float min = arr[0];
+        for (float l : arr)
+            min = Math.max(l, min);
+        return min;
+    }
+    
+    public static double max(double... arr) {
+        if (arr.length == 0) return 0;
+        double min = arr[0];
+        for (double l : arr)
+            min = Math.max(l, min);
+        return min;
+    }
+    
+    public static short max(short... arr) {
+        if (arr.length == 0) return 0;
+        int min = arr[0];
+        for (int l : arr)
+            min = Math.max(l, min);
+        return (short) min;
+    }
+    
+    public static byte max(byte... arr) {
+        if (arr.length == 0) return 0;
+        int min = arr[0];
+        for (int l : arr)
+            min = Math.max(l, min);
+        return (byte) min;
+    }
+    
+    public static int max(int... arr) {
+        if (arr.length == 0) return 0;
+        int min = arr[0];
+        for (int l : arr)
+            min = Math.max(l, min);
+        return min;
+    }
+    
+    public static long max(long... arr) {
+        if (arr.length == 0) return 0;
+        long min = arr[0];
+        for (long l : arr)
+            min = Math.max(l, min);
+        return min;
+    }
+    
+    public static float min(float... arr) {
+        if (arr.length == 0) return 0;
+        float min = arr[0];
+        for (float l : arr)
+            min = Math.min(l, min);
+        return min;
+    }
+    
+    public static double min(double... arr) {
+        if (arr.length == 0) return 0;
+        double min = arr[0];
+        for (double l : arr)
+            min = Math.min(l, min);
+        return min;
+    }
+    
+    public static short min(short... arr) {
+        if (arr.length == 0) return 0;
+        int min = arr[0];
+        for (short l : arr)
+            min = Math.min(l, min);
+        return (short) min;
+    }
+    
+    public static byte min(byte... arr) {
+        if (arr.length == 0) return 0;
+        int min = arr[0];
+        for (byte l : arr)
+            min = Math.min(l, min);
+        return (byte) min;
+    }
+    
+    public static int min(int... arr) {
+        if (arr.length == 0) return 0;
+        int min = arr[0];
+        for (int l : arr)
+            min = Math.min(l, min);
+        return min;
+    }
+    
+    public static long min(long... arr) {
+        if (arr.length == 0) return 0;
+        long min = arr[0];
+        for (long l : arr)
+            min = Math.min(l, min);
+        return min;
+    }
+    
+    //primitive
+    public static double avg(boolean... arr) {
+        long sum = 0;
+        int length = 0;
+        for (boolean l : arr) {
+            sum += l ? 1 : 0;
+            length++;
+        }
+        return (double) sum / length;
+    }
+    
+    public static double avg(double... arr) {
+        double sum = 0;
+        int length = 0;
+        for (double l : arr) {
+            sum += l;
+            length++;
+        }
+        return (double) sum / length;
+    }
+    
+    public static double avg(float... arr) {
+        double sum = 0;
+        int length = 0;
+        for (double l : arr) {
+            sum += l;
+            length++;
+        }
+        return (double) sum / length;
+    }
+    
+    public static double avg(byte... arr) {
+        long sum = 0;
+        int length = 0;
+        for (long l : arr) {
+            sum += l;
+            length++;
+        }
+        return (double) sum / length;
+    }
+    
+    public static double avg(char... arr) {
+        long sum = 0;
+        int length = 0;
+        for (long l : arr) {
+            sum += l;
+            length++;
+        }
+        return (double) sum / length;
+    }
+    
+    public static double avg(int... arr) {
+        long sum = 0;
+        int length = 0;
+        for (long l : arr) {
+            sum += l;
+            length++;
+        }
+        return (double) sum / length;
+    }
+    
+    public static double avg(short... arr) {
+        long sum = 0;
+        int length = 0;
+        for (long l : arr) {
+            sum += l;
+            length++;
+        }
+        return (double) sum / length;
+    }
+    
+    public static double avg(long... arr) {
+        long sum = 0;
+        int length = 0;
+        for (long l : arr) {
+            sum += l;
+            length++;
+        }
+        return (double) sum / length;
+    }
+    
+    public static double avg(Iterator<Long> arr) {
+        long sum = 0;
+        int length = 0;
+        
+        while (arr.hasNext()) {
+            long l = arr.next();
+            sum += l;
+            length++;
+        }
+        return (double) sum / length;
     }
     
     public static double avg(Iterable<Long> arr) {
