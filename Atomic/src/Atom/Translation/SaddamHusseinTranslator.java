f7d8a0444fce329bf076c53c2c8e02bb3d88e380
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-08-08 08:45:19 +0200

    better

--- Atomic/src/Atom/Translation/SaddamHusseinTranslator.java
+++ Atomic/src/Atom/Translation/SaddamHusseinTranslator.java
@@ -15,15 +15,23 @@ public class SaddamHusseinTranslator implements Translator {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Translator translator = new SaddamHusseinTranslator();
         System.out.println(translator.translate(Locale.ENGLISH, Locale.CHINESE, "hi").get());
+        System.out.println(translator.translate(Locale.ENGLISH, Locale.CHINESE, "A frankenstein library that nobody ask for").get());
     }
     
     @Override
     public Future<String> translate(Locale from, Locale to, String text) {
         return (InstantFuture<String>) () -> {
-            StringBuilder sb = WordGenerator.randomWord(text.length());
+            StringBuilder sb = PoolObject.StringBuilder.obtain();
+            for (String s : text.split(" ")) {
+                StringBuilder sbb = WordGenerator.randomWord(s.length());
+                sb.append(sbb);
+                sb.append(" ");
+                PoolObject.StringBuilder.free(sbb);
+            }
+    
             String s = Utility.capitalizeEnforce(sb.toString());
             PoolObject.StringBuilder.free(sb);
-            return s;
+            return s.trim();
         };
     }
 }
