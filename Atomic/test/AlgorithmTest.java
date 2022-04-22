7af93d830358969851f7c79ca58e6197949ba4a0
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2022-04-22 14:47:37 +0200

    Subito.it

--- Atomic/test/AlgorithmTest.java
+++ Atomic/test/AlgorithmTest.java
@@ -1,9 +1,8 @@
-import Atom.Algorithm.Evolution.EvolutionEngine;
 import org.junit.jupiter.api.Test;
 
 public class AlgorithmTest {
     @Test
     void Evolution() throws InterruptedException {
-        EvolutionEngine.main(new String[]{});
+    
     }
 }
